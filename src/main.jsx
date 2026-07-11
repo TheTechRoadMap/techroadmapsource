a1c84215a7b260191db807922c27a248613eb8d3
Mosia Marate <221047469@stud.cut.ac.za>
2026-07-11 20:30:31 +0200

    Add comment to clarify root creation in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.jsx';
 
+// Create a root.
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
